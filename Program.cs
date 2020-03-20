d3b826feac1ddbc4c792e9a216fa444422ad383a
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-03-20 13:59:16 +1300

    put some code in

--- Program.cs
+++ Program.cs
@@ -6,17 +6,24 @@ namespace CS_HiTechScrape
 {
     class Program
     {
+
+        static HtmlDocument GetDocument(string url)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument document = web.Load(url);
+            return document; 
+        }
         static List<string> LoadYear()
         {
             List<string> years_url = new List<string>(); 
-            string url = "https://www.hitech.org.nz/awards/finalists/"; 
-            HtmlWeb web = new HtmlWeb();
-            HtmlDocument years_dom = web.Load(url); 
+            string url = "https://www.hitech.org.nz/awards/finalists/";
+            HtmlDocument years_dom = GetDocument(url); 
             HtmlNode main_nodes = years_dom.DocumentNode.SelectSingleNode("//*[@id='app']/main");
             HtmlNodeCollection year_nodes = main_nodes.SelectNodes("//*[@id='app']/main/div/div[@class='columns']/a");
             foreach (HtmlNode a in year_nodes)
             {
-                string href = a.Attributes.
+                string href = a.Attributes["href"].Value;
+                years_url.Add("https://www.hitech.org.nz" + href); 
             }
             return years_url; 
         }
